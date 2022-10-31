65672c56f10c50d6b48fc9533aa3fe0d52c5d5e6
D4vid134 <davidqiu29@icloud.com>
2022-10-31 04:16:59 -0500

    r

--- R-Type Remake/Assets/Scripts/PlayerProjectile.cs
+++ R-Type Remake/Assets/Scripts/PlayerProjectile.cs
@@ -4,6 +4,54 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
+
+    [SerializeField] int power;
+
+    //void Fire1()
+    //{
+    //    Rigidbody2D playerProjectile = Instantiate(playerProjectilePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation) as Rigidbody2D;
+
+    //    playerProjectile.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed);
+
+    //    coolDown = Time.time + attackSpeed;
+    //}
+
+    //void Fire2()
+    //{
+    //    Rigidbody2D playerProjectile = Instantiate(playerProjectilePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation) as Rigidbody2D;
+
+    //    playerProjectile.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed);
+
+    //    coolDown = Time.time + attackSpeed;
+    //}
+
+    //void Fire3()
+    //{
+    //    Rigidbody2D playerProjectile = Instantiate(playerProjectilePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation) as Rigidbody2D;
+
+    //    playerProjectile.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed);
+
+    //    coolDown = Time.time + attackSpeed;
+    //}
+
+    //void Fire4()
+    //{
+    //    Rigidbody2D playerProjectile = Instantiate(playerProjectilePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation) as Rigidbody2D;
+
+    //    playerProjectile.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed);
+
+    //    coolDown = Time.time + attackSpeed;
+    //}
+
+    //void FireMax()
+    //{
+    //    Rigidbody2D playerProjectile = Instantiate(playerProjectilePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation) as Rigidbody2D;
+
+    //    playerProjectile.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed);
+
+    //    coolDown = Time.time + attackSpeed;
+    //}
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +62,17 @@ public class PlayerProjectile : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            power = power - 1;
+            // Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            // Player Dies
+        }
+
     }
 }
